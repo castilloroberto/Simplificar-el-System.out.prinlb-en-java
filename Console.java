91ab0d86b3317f5b8820ca8295cc58f8a78b075a
castillo <robertocastillo945@gmail.com>
2020-12-26 16:26:37 -0600

    simplificar el System_out_prinln() en java

---
+++ Console.java
@@ -0,0 +1,12 @@
+public class Console {
+    
+    static void log(String x){
+        System.out.println(x);
+    }
+    static void log(int x){
+        System.out.println(x);
+    }
+    static void log(double x){
+        System.out.println(x);
+    }
+}
---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main{
+    public static void main(String args[]){
+       
+        //forma corta
+
+        Console.log("altura:" + 1.85);//float u double
+        Console.log("entero: " + 50);
+
+    }
+}
